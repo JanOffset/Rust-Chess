546e9f97f6aa293c24226df657f5b0013ff1ed78
Trayan <trayanshterionov7@gmail.com>
2025-01-12 15:01:10 +0000

    implementing position & destination logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-
+use std::io;
 fn main() {
-    let row = ['a','b','c','d','e','f','g','h'];
-    let col = [1,2,3,4,5,6,7, 8];
-    let mut positions = Vec::new();
+    loop {
+    let row: [char; 8] = ['a','b','c','d','e','f','g','h'];
+    let col: [i32; 8] = [1,2,3,4,5,6,7, 8];
+    let mut positions: Vec<String> = Vec::new();
+    println!("Your Move:");
+    let mut input: String = String::new();
+    let input: &str = input.trim();
+    let moves: Vec<String> = input.split_whitespace().collect();
+    // this input is for the color
+    
+    io::stdin().read_line(&mut input).unwrap();
+    //bl or wh for color; h3, h4 positions; wh h3 h4; color current_position destination
+
+
     for letter in row.iter() {
         for number in col.iter() {
             let position = format!("{}{}", letter, number );
@@ -11,12 +22,6 @@ fn main() {
         }
         println!()
     }
-  /*   for pos in &positions {
-        for _ in 0..8
-        {
-            print!("{}", pos.to_string())
-        }
-        println!();
     }
-*/
+    break;
 }
